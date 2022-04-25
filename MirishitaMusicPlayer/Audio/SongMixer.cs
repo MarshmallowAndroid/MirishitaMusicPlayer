25bf2f104dcdf899cd6b927b1f90dd0b694d404f
Jacob Tarun <jacobtarun782@outlook.com>
2022-04-26 01:12:48 +0800

    Improve song mixer.

--- MirishitaMusicPlayer/Audio/SongMixer.cs
+++ MirishitaMusicPlayer/Audio/SongMixer.cs
@@ -8,20 +8,25 @@ namespace MirishitaMusicPlayer.Audio
     internal class SongMixer : ISampleProvider, IDisposable
     {
         private readonly AcbWaveStream backgroundWaveStream;
+        private readonly AcbWaveStream backgroundExWaveStream;
         private readonly ISampleProvider backgroundSampleProvider;
+        private readonly ISampleProvider backgroundExSampleProvider;
 
         private readonly List<VoiceTrack> voiceSampleProviders;
-        private readonly VoiceTrack backgroundEx;
 
-        private readonly List<(long Sample, int ActiveSingers)> triggers = new();
+        private readonly List<(long Sample, int ActiveSingers)> volumeTriggers = new();
+        private readonly List<(long Sample, bool Active)> exTriggers = new();
 
         private readonly int voiceChannelCount;
         private readonly int channelDivide;
 
-        private long currentSample = 0;
-        private int nextTriggerIndex = 0;
+        private long currentVolumeSample = 0;
+        private long currentExSample = 0;
+        private int nextVolumeTriggerIndex = 0;
+        private int nextExTriggerIndex = 0;
 
         private float multiplier = 1.0f;
+        private bool exActive = false;
 
         private bool disposing;
 
@@ -34,11 +39,15 @@ namespace MirishitaMusicPlayer.Audio
             backgroundWaveStream = bgmAcb;
             backgroundSampleProvider = bgmAcb.ToSampleProvider();
 
+            // Add the extra BGM, if any
             if (bgmExAcb != null)
-                backgroundEx = new VoiceTrack(bgmExAcb, muteScenarios, 5, true);
+            {
+                backgroundExWaveStream = bgmExAcb;
+                backgroundExSampleProvider = backgroundExWaveStream.ToSampleProvider();
+            }
 
+            // Add our voice SampleProviders
             voiceSampleProviders = new();
-
             if (voiceAcbs != null)
             {
                 int voiceIndex = 0;
@@ -58,16 +67,20 @@ namespace MirishitaMusicPlayer.Audio
                 }
             }
 
+            // Convert our mute scenarios into triggers
             foreach (var muteScenario in muteScenarios)
             {
-                triggers.Add((AbsTimeToSamples(muteScenario.AbsTime), GetActiveSingers(muteScenario.Mute)));
+                volumeTriggers.Add((AbsTimeToSamples(muteScenario.AbsTime), GetActiveSingers(muteScenario.Mute)));
+
+                if (muteScenario.Mute.Length == 6 && backgroundExSampleProvider != null)
+                {
+                    exTriggers.Add((AbsTimeToSamples(muteScenario.AbsTime), muteScenario.Mute[5] == 1));
+                }
             }
 
             // For converting mono voices to stereo
-
             if (voiceSampleProviders.Count > 0)
                 voiceChannelCount = voiceSampleProviders[0].WaveFormat.Channels; // Get the channel count of the voices
-
             if (voiceChannelCount != 2)
                 channelDivide = 2; // Read half the bytes (count / 2), one half for each stereo channel
             else
@@ -89,16 +102,18 @@ namespace MirishitaMusicPlayer.Audio
         public void Reset()
         {
             backgroundWaveStream.Position = 0;
-            currentSample = 0;
-            nextTriggerIndex = 0;
 
-            if (backgroundEx != null)
-                backgroundEx.Reset();
+            if (backgroundExWaveStream != null)
+                backgroundExWaveStream.Position = 0;
 
             foreach (var voice in voiceSampleProviders)
             {
                 voice.Reset();
             }
+
+            currentVolumeSample = 0;
+            currentExSample = 0;
+            nextVolumeTriggerIndex = 0;
         }
 
         public void Seek(float seconds)
@@ -115,33 +130,37 @@ namespace MirishitaMusicPlayer.Audio
                 (backgroundWaveStream.WaveFormat.BitsPerSample / 8 *
                 backgroundWaveStream.WaveFormat.SampleRate *
                 backgroundWaveStream.WaveFormat.Channels));
-            currentSample = backgroundWaveStream.Position /
-                backgroundWaveStream.WaveFormat.Channels /
-                (backgroundWaveStream.WaveFormat.BitsPerSample / 8);
-            nextTriggerIndex = 0;
 
-            if (backgroundEx != null)
-                backgroundEx.Seek(seconds);
+            if (backgroundExWaveStream != null)
+                backgroundExWaveStream.Position = backgroundWaveStream.Position;
 
             foreach (var voice in voiceSampleProviders)
             {
                 voice.Seek(seconds);
             }
+
+            currentVolumeSample = backgroundWaveStream.Position /
+                backgroundWaveStream.WaveFormat.Channels /
+                (backgroundWaveStream.WaveFormat.BitsPerSample / 8);
+            currentExSample = backgroundWaveStream.Position /
+                backgroundWaveStream.WaveFormat.Channels /
+                (backgroundWaveStream.WaveFormat.BitsPerSample / 8);
+            nextVolumeTriggerIndex = 0;
         }
 
         public int Read(float[] buffer, int offset, int count)
         {
             if (disposing) return 0;
 
+            // Clear buffer
             for (int i = 0; i < count; i++)
             {
                 buffer[i] = 0;
             }
 
             float[] bufferMain = new float[count];
-            float[] bufferEx = new float[count];
 
-            // Mix voices
+            // Read and mix voices
             for (int i = 0; i < voiceSampleProviders.Count; i++)
             {
                 voiceSampleProviders[i].Read(bufferMain, offset, count / channelDivide);
@@ -159,44 +178,40 @@ namespace MirishitaMusicPlayer.Audio
                 }
             }
 
-
-            int finalRead;
-
-            // Read from background music
-            int backgroundRead = backgroundSampleProvider.Read(bufferMain, offset, count);
-
             // Adjust volume of voices
             //
             // We need the number of real voices enabled, for cases where
             // the VoiceControl array is less than sample provider count
             //
             // Necessary for proper voice volume adjustment
-            for (int i = 0; i < backgroundRead / WaveFormat.Channels; i++)
+            for (int i = offset; i < count / backgroundWaveStream.WaveFormat.Channels; i++)
             {
-                while (currentSample >= triggers[nextTriggerIndex].Sample)
+                while (currentVolumeSample >= volumeTriggers[nextVolumeTriggerIndex].Sample)
                 {
-                    multiplier = (float)1 / (triggers[nextTriggerIndex].ActiveSingers + 1) + 0.10f;
+                    multiplier = (float)1 / (volumeTriggers[nextVolumeTriggerIndex].ActiveSingers + 1) + 0.10f;
 
-                    if (nextTriggerIndex < triggers.Count - 1) nextTriggerIndex++;
+                    if (nextVolumeTriggerIndex < volumeTriggers.Count - 1) nextVolumeTriggerIndex++;
                     else break;
                 }
 
-                for (int j = 0; j < WaveFormat.Channels; j++)
+                for (int j = 0; j < backgroundWaveStream.WaveFormat.Channels; j++)
                 {
                     buffer[i * WaveFormat.Channels + j] *= multiplier;
                 }
 
-                currentSample++;
+                currentVolumeSample++;
             }
+
+            // Read from background music
+            int backgroundRead = backgroundSampleProvider.Read(bufferMain, offset, count);
 
             // If available, read from extra (secondary) background music
+            float[] bufferEx = new float[count];
             int backgroundExRead = 0;
-            if (backgroundEx != null)
+            if (backgroundExSampleProvider != null)
             {
-                backgroundExRead = backgroundEx.Read(bufferEx, offset, count);
+                backgroundExRead = backgroundExSampleProvider.Read(bufferEx, offset, count);
             }
-
-            float[] sourceBuffer;
 
             // If the extra background music is specified and voice control is available and has 6 voices,
             // use the extra background music instead of the original background music if the 6th voice
@@ -204,23 +219,31 @@ namespace MirishitaMusicPlayer.Audio
             //
             // The 6th voice or extra background music is just a pre-mixed version of all the idols plus
             // the background music.
-            if (backgroundEx != null && backgroundEx.Singing)
-            {
-                sourceBuffer = bufferEx;
-                finalRead = backgroundExRead;
-            }
-            else
-            {
-                sourceBuffer = bufferMain;
-                finalRead = backgroundRead;
-            }
-
-            // Plop the data into the output buffer
-            for (int i = 0; i < count; i++)
+            for (int i = offset; i < count / backgroundWaveStream.WaveFormat.Channels; i++)
             {
                 if (MuteBackground) break;
-                buffer[i] += sourceBuffer[i];
+
+                if (backgroundExWaveStream != null)
+                {
+                    while (currentExSample >= exTriggers[nextExTriggerIndex].Sample)
+                    {
+                        exActive = exTriggers[nextExTriggerIndex].Active;
+
+                        if (nextExTriggerIndex < exTriggers.Count - 1) nextExTriggerIndex++;
+                        else break;
+                    }
+                }
+
+                for (int j = 0; j < WaveFormat.Channels; j++)
+                {
+                    int index = i * WaveFormat.Channels + j;
+                    buffer[index] += exActive ? bufferEx[index] : bufferMain[index];
+                }
+
+                currentExSample++;
             }
+
+            int finalRead = exActive ? backgroundExRead : backgroundRead;
 
             // Set our end flag to true
             if (finalRead != count) HasEnded = true;
@@ -235,8 +258,8 @@ namespace MirishitaMusicPlayer.Audio
 
             backgroundWaveStream.Dispose();
 
-            if (backgroundEx != null)
-                backgroundEx.Dispose();
+            if (backgroundExWaveStream != null)
+                backgroundExWaveStream.Dispose();
 
             foreach (var voice in voiceSampleProviders)
             {
