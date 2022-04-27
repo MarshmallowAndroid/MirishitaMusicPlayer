8b172915fa4fbefb36f25cca604fdb6b788e0038
Jacob Tarun <jacobtarun782@outlook.com>
2022-04-27 09:09:08 +0800

    Improve idol class.

--- MirishitaMusicPlayer/Idol.cs
+++ MirishitaMusicPlayer/Idol.cs
@@ -1,80 +1,85 @@
-﻿namespace MirishitaMusicPlayer
+﻿using System.Collections.Generic;
+
+namespace MirishitaMusicPlayer
 {
-    internal class Idol
+    public class Idol
     {
-        public Idol(string idolNameID)
+        private static readonly Dictionary<int, string[]> idols = new()
         {
-            IdolNameID = idolNameID;
+            { 001, new[] { "001har", "001har", "Haruka Amami" } },
+            { 002, new[] { "002chi", "002chi", "Chihaya Kisaragi" } },
+            { 003, new[] { "003mik", "003mik", "Miki Hoshii" } },
+            { 004, new[] { "004yuk", "004yuk", "Yukiho Hagiwara" } },
+            { 005, new[] { "005yay", "005yay", "Yayoi Takatsuki" } },
+            { 006, new[] { "006mak", "006mak", "Makoto Kikuchi" } },
+            { 007, new[] { "007ior", "007ior", "Iori Minase" } },
+            { 008, new[] { "008tak", "008tak", "Takane Shijou" } },
+            { 009, new[] { "009rit", "009rit", "Ritsuko Akizuki" } },
+            { 010, new[] { "010azu", "010azu", "Azusa Miura" } },
+            { 011, new[] { "011ami", "011maw", "Ami Futami" } },
+            { 012, new[] { "012mam", "012mam", "Mami Futami" } },
+            { 013, new[] { "013hib", "013hib", "Hibiki Ganaha" } },
+            { 014, new[] { "014mir", "014mir", "Mirai Kasuga" } },
+            { 015, new[] { "015siz", "015siz", "Shizuka Mogami" } },
+            { 016, new[] { "016tsu", "016tsu", "Tsubasa Ibuki" } },
+            { 017, new[] { "017kth", "017kth", "Kotoha Tanaka" } },
+            { 018, new[] { "018ele", "018ele", "Elena Shimabara" } },
+            { 019, new[] { "019min", "019min", "Minako Satake" } },
+            { 020, new[] { "020meg", "020meg", "Megumi Tokoro" } },
+            { 021, new[] { "021mat", "021mat", "Matsuri Tokugawa" } },
+            { 022, new[] { "022ser", "022ser", "Serika Hakozaki" } },
+            { 023, new[] { "023aka", "023aka", "Akane Nonohara" } },
+            { 024, new[] { "024ann", "024ann", "Anna Mochizuki" } },
+            { 025, new[] { "025roc", "025roc", "Roco Handa" } },
+            { 026, new[] { "026yur", "026yur", "Yuriko Nanao" } },
+            { 027, new[] { "027say", "027say", "Sayoko Takayama" } },
+            { 028, new[] { "028ari", "028ari", "Arisa Matsuda" } },
+            { 029, new[] { "029umi", "029umi", "Umi Kousaka" } },
+            { 030, new[] { "030iku", "030iku", "Iku Nakatani" } },
+            { 031, new[] { "031tom", "031tom", "Tomoka Tenkubashi" } },
+            { 032, new[] { "032emi", "032emi", "Emily Stewart" } },
+            { 033, new[] { "033sih", "033sih", "Shiho Kitazawa" } },
+            { 034, new[] { "034ayu", "034ayu", "Ayumu Maihama" } },
+            { 035, new[] { "035hin", "035hin", "Hinata Kinoshita" } },
+            { 036, new[] { "036kan", "036kan", "Kana Yabuki" } },
+            { 037, new[] { "037nao", "037nao", "Nao Yokoyama" } },
+            { 038, new[] { "038chz", "038chz", "Chizuru Nikaido" } },
+            { 039, new[] { "039kon", "039kon", "Konomi Baba" } },
+            { 040, new[] { "040tam", "040tam", "Tamaki Ogami" } },
+            { 041, new[] { "041fuk", "041fuk", "Fuka Toyokawa" } },
+            { 042, new[] { "042miy", "042miy", "Miya Miyao" } },
+            { 043, new[] { "043nor", "043nor", "Noriko Fukuda" } },
+            { 044, new[] { "044miz", "044miz", "Mizuki Makabe" } },
+            { 045, new[] { "045kar", "045kar", "Karen Shinomiya" } },
+            { 046, new[] { "046rio", "046rio", "Rio Momose" } },
+            { 047, new[] { "047sub", "047sub", "Subaru Nagayoshi" } },
+            { 048, new[] { "048rei", "048rei", "Reika Kitakami" } },
+            { 049, new[] { "049mom", "049mom", "Momoko Suou" } },
+            { 050, new[] { "050jul", "050jul", "Julia" } },
+            { 051, new[] { "051tmg", "051tmg", "Tsumugi Shiraishi" } },
+            { 052, new[] { "052kao", "052kao", "Kaori Sakuramori" } },
+        };
+
+        public Idol(int idolIDNumber)
+        {
+            IdolIDNumber = idolIDNumber;
         }
 
-        public string IdolNameID { get; }
+        public Idol(string idolNameID)
+        {
+            IdolIDNumber = int.Parse(idolNameID[0..3]);
+        }
 
-        public int IdolIDNumber => int.Parse(IdolNameID[0..3]);
+        public string IdolNameID => idols[IdolIDNumber][0];
 
-        public string IdolNameFull => IdolNameIDToFull(IdolNameID);
+        public string IdolAudioNameID => idols[IdolIDNumber][1];
+
+        public int IdolIDNumber { get; }
+
+        public string IdolNameFull => idols[IdolIDNumber][2];
 
         public string IdolFirstName => IdolNameFull.Split(' ')[0].Trim();
 
         public string IdolLastName => IdolNameFull.Split(' ')[1].Trim();
-
-        private static string IdolNameIDToFull(string nameID)
-        {
-            return nameID switch
-            {
-                "001har" => "Haruka Amami",
-                "002chi" => "Chihaya Kisaragi",
-                "003mik" => "Miki Hoshii",
-                "004yuk" => "Yukiho Hagiwara",
-                "005yay" => "Yayoi Takatsuki",
-                "006mak" => "Makoto Kikuchi",
-                "007ior" => "Iori Minase",
-                "008tak" => "Takane Shijou",
-                "009rit" => "Ritsuko Akizuki",
-                "010azu" => "Azusa Miura",
-                "011maw" => "Ami Futami",
-                "012mam" => "Mami Futami",
-                "013hib" => "Hibiki Ganaha",
-                "014mir" => "Mirai Kasuga",
-                "015siz" => "Shizuka Mogami",
-                "016tsu" => "Tsubasa Ibuki",
-                "017kth" => "Kotoha Tanaka",
-                "018ele" => "Elena Shimabara",
-                "019min" => "Minako Satake",
-                "020meg" => "Megumi Tokoro",
-                "021mat" => "Matsuri Tokugawa",
-                "022ser" => "Serika Hakozaki",
-                "023aka" => "Akane Nonohara",
-                "024ann" => "Anna Mochizuki",
-                "025roc" => "Roco Handa",
-                "026yur" => "Yuriko Nanao",
-                "027say" => "Sayoko Takayama",
-                "028ari" => "Arisa Matsuda",
-                "029umi" => "Umi Kousaka",
-                "030iku" => "Iku Nakatani",
-                "031tom" => "Tomoka Tenkubashi",
-                "032emi" => "Emily Stewart",
-                "033sih" => "Shiho Kitazawa",
-                "034ayu" => "Ayumu Maihama",
-                "035hin" => "Hinata Kinoshita",
-                "036kan" => "Kana Yabuki",
-                "037nao" => "Nao Yokoyama",
-                "038chz" => "Chizuru Nikaido",
-                "039kon" => "Konomi Baba",
-                "040tam" => "Tamaki Ogami",
-                "041fuk" => "Fuka Toyokawa",
-                "042miy" => "Miya Miyao",
-                "043nor" => "Noriko Fukuda",
-                "044miz" => "Mizuki Makabe",
-                "045kar" => "Karen Shinomiya",
-                "046rio" => "Rio Momose",
-                "047sub" => "Subaru Nagayoshi",
-                "048rei" => "Reika Kitakami",
-                "049mom" => "Momoko Suou",
-                "050jul" => "Julia",
-                "051tmg" => "Tsumugi Shiraishi",
-                "052kao" => "Kaori Sakuramori",
-                _ => "Unknown",
-            };
-        }
     }
 }
