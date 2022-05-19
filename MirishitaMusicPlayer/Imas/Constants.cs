d549647391faf42098dfd2221c1e37bb97a1133f
Jacob Tarun <jacobtarun782@outlook.com>
2022-05-19 16:37:36 +0800

    Update constants.

--- MirishitaMusicPlayer/Imas/Constants.cs
+++ MirishitaMusicPlayer/Imas/Constants.cs
@@ -2,10 +2,22 @@
 {
     public enum ScenarioType : int
     {
+        Glowsticks = 1,
+
+        ShowTitle = 2,
+        HideTitle = 3,
+
         ShowLyrics = 11,
         HideLyrics = 12,
+
         LipSync = 43,
         Expression = 45,
+
+        Lights = 51,
+
+        ShowTouch = 56,
+        MergeTouch = 57,
+
         Mute = 59
     }
 }
