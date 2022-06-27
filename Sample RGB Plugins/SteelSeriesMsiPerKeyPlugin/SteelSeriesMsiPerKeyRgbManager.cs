3ef4b73bb0c4bc9022f0d38f844d082a30e09dee
Jacob Tarun <jacobtarun782@outlook.com>
2022-06-27 17:10:28 +0800

    Add SteelSeries GameSense RGB plugin.

---
+++ Sample RGB Plugins/SteelSeriesMsiPerKeyPlugin/SteelSeriesMsiPerKeyRgbManager.cs
@@ -0,0 +1,48 @@
+﻿using MirishitaMusicPlayer.RgbPluginBase;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SteelSeriesMsiPerKeyPlugin
+{
+    public class SteelSeriesMsiPerKeyRgbManager : IRgbManager
+    {
+        private HttpClient httpClient;
+
+        public SteelSeriesMsiPerKeyRgbManager()
+        {
+            var programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            var coreProps = Path.Combine(programData, "SteelSeries\\SteelSeries Engine 3\\coreProps.json");
+            var address = (string?)JObject.Parse(File.ReadAllText(coreProps))["address"] ?? "";
+
+            if (address == "") throw new Exception("Could not get GameSense SDK address.");
+
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://" + address)
+            };
+        }
+
+        public IDeviceConfiguration[] DeviceConfigurations => throw new NotImplementedException();
+
+        public bool Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Form? GetSettingsForm(IEnumerable<int> targets)
+        {
+            return null;
+        }
+
+        public void UpdateRgb(int target, Color color, Color color2, Color color3, float duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
